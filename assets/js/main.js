324deab0e7c519c27e527dccf6e4818fa9e95c1b
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2021-01-08 11:15:47 -0500

    linked social media

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,4 +45,6 @@ sr.reveal(".skills__data", { interval: 200 });
 sr.reveal(".skills__img", { delay: 400 });
 
 /*===== SCROLL WORK =====*/
+sr.reveal(".work__img", { interval: 200 });
 /*===== SCROLL CONTACT =====*/
+sr.reveal(".contact__input", { interval: 200 });
